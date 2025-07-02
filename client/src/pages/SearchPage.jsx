9613138b7fd10e49f3b48c95674138cf9a8cd07b
ANKUSH JAIN <133674486+AnkushjainAj@users.noreply.github.com>
2025-07-02 18:11:07 +0530

    fix responsiveness

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -23,14 +23,21 @@ const SearchPage = () => {
   };
 
   return (
-    <div>
-      <div className="flex justify-between ml-[9rem] mr-[9rem] mt-8">
-        <FilterSidebar applyFilters={applyFilters} />
-        <ProductList
-          products={
-            filteredResults.length > 0 ? filteredResults : search.results
-          }
-        />
+    <div className="px-4 md:px-36 mt-8">
+      <div className="flex flex-col lg:flex-row gap-6">
+        {/* Sidebar will be full width on mobile, fixed width on desktop */}
+        <div className="w-full lg:w-1/4">
+          <FilterSidebar applyFilters={applyFilters} />
+        </div>
+
+        {/* Product list takes full width on mobile, remaining space on desktop */}
+        <div className="w-full lg:w-3/4">
+          <ProductList
+            products={
+              filteredResults.length > 0 ? filteredResults : search.results
+            }
+          />
+        </div>
       </div>
     </div>
   );
